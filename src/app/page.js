ce1465d2bb8bf8d66ab60f52562ca6a813b98e29
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2023-06-14 01:49:05 +0530

    Linking and Navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
-import styles from './page.module.css';
+'use client';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+  const navigate = (route) => {
+    router.push(route);
+  };
   return (
-    <main className={styles.main}>
-      <h1>Basic Routing</h1>
+    <main>
+      <h1>Linking and Navigation</h1>
+      <Link href='./login'>Go to Login Page</Link>
+      <br />
+      <Link href='./about'>Go to About Page</Link>
+      <br />
+      <button onClick={() => navigate('/login')}>Go to Login Page</button>
+      <br />
+      <button onClick={() => navigate('/about')}>Go to About Page</button>
     </main>
   );
 }
