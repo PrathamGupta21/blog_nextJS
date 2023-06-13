dde650969a552fc6ed28b657777b1ae0ca07867e
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2023-06-14 01:30:28 +0530

    Basic Routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,9 @@
-'use client';
-import { useState } from 'react';
 import styles from './page.module.css';
 
 export default function Home() {
-  const [name, setName] = useState('Pratham');
-
-  const clickHandler = () => {
-    setName('Gupta');
-  };
-
   return (
     <main className={styles.main}>
-      <h1>Events, Function and State - {name}</h1>
-      <button onClick={clickHandler}>Click Me</button>
+      <h1>Basic Routing</h1>
     </main>
   );
 }
